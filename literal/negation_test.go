cddf17a5427e118eb40f45f123c905f92bab8d59
Shane Aston <astonshane@gmail.com>
2015-09-01 20:26:38 -0400

    modified test of literal.Negation()

--- literal/negation_test.go
+++ literal/negation_test.go
@@ -15,8 +15,14 @@ func TestNegation(t *testing.T) {
 		l := Literal{Name: c.inName, Negated: c.inNegated}
 		neg := l.Negation()
 		got := neg.Print()
+		//test that the negation is what we want
 		if got != c.want {
 			t.Errorf("Negation(%q) == %q, want %q", l.Print(), got, c.want)
+		}
+		//test that the negation of the negation is what we statted with
+		original := neg.Negation()
+		if l.Print() != original.Print() {
+			t.Errorf("Negation(Negation(%q)) == %q, want %q", l.Print(), original.Print(), l.Print())
 		}
 	}
 }
