9cf80713335112f15ad701e6ace4301f6c0c1457
Shane Aston <astonshane@gmail.com>
2015-09-07 19:08:52 -0400

    fixed tests for ClauseSet.Copy()

--- clauseset/copy_test.go
+++ clauseset/copy_test.go
@@ -1,12 +1,47 @@
 package clauseset
 
 import (
-	//"davisputnam/clause"
-	//"davisputnam/literal"
+	"davisputnam/clause"
 	"testing"
 )
 
 func TestCopy(t *testing.T) {
-	//TODO
+	_, one, two, _ := clause.ConstructTestClauses()
+
+	//case0 -- empty ClauseSet, don't change
+	cs := ClauseSet{}
+	copy := cs.Copy()
+
+	if !Equals(cs, copy) {
+		t.Errorf("original (%q) != copy (%q)", cs, copy)
+	}
+
+	//case1 -- empty ClauseSet, chagne copy
+	cs = ClauseSet{}
+	copy = cs.Copy()
+	copy.Append(one)
+
+	if Equals(cs, copy) {
+		t.Errorf("original (%q) == Copy().Append(%q) (%q)", cs, one, copy)
+	}
+
+	//case2 -- 1 thing in ClauseSet, don't change
+	cs = ClauseSet{}
+	cs.Append(two)
+	copy = cs.Copy()
+
+	if !Equals(cs, copy) {
+		t.Errorf("original (%q) != copy (%q)", cs, copy)
+	}
+
+	//case3 -- 1 thing in ClauseSet, chagne copy
+	cs = ClauseSet{}
+	cs.Append(two)
+	copy = cs.Copy()
+	copy.Append(one)
+
+	if Equals(cs, copy) {
+		t.Errorf("original (%q) == Copy().Append(%q, %q) (%q)", cs, one, two, copy)
+	}
 
 }
