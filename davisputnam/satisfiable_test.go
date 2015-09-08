593b98fa57faff2fb4f23055bd49c641be41e4a9
Shane Aston <astonshane@gmail.com>
2015-09-07 21:46:55 -0400

    tests for Satisfiable()

---
+++ davisputnam/satisfiable_test.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"davisputnam/clause"
+	"davisputnam/clauseset"
+	"davisputnam/literal"
+	"testing"
+)
+
+func SatValid(t *testing.T) {
+	a := literal.Literal{"A", false}
+	na := a.Negation()
+	n := literal.Literal{"N", false}
+	nn := n.Negation()
+	q := literal.Literal{"Q", false}
+	nq := q.Negation()
+
+	one := clause.Clause{}
+	one.Append(a)
+
+	two := clause.Clause{}
+	two.Append(nq)
+
+	three := clause.Clause{}
+	three.Append(nn)
+
+	four := clause.Clause{}
+	four.Append(na)
+	four.Append(n)
+	four.Append(q)
+
+	CS := clauseset.ClauseSet{}
+	CS.Append(one)
+	CS.Append(two)
+	CS.Append(three)
+	CS.Append(four)
+
+	sat := Satisfiable(CS)
+	if sat {
+		t.Errorf("ClauseSet %q was Satisfiable, so it is an invalid argument (it should be valid)", CS)
+	}
+
+}
+
+func SatInvalid(t *testing.T) {
+	na := literal.Literal{"A", true}
+	b := literal.Literal{"B", false}
+
+	one := clause.Clause{}
+	one.Append(na)
+
+	two := clause.Clause{}
+	two.Append(b)
+
+	three := clause.Clause{}
+	three.Append(na)
+	three.Append(b)
+
+	CS := clauseset.ClauseSet{}
+	CS.Append(one)
+	CS.Append(two)
+	CS.Append(three)
+
+	sat := Satisfiable(CS)
+	if !sat {
+		t.Errorf("ClauseSet %q was not Satisfiable, so it is a valid argument (it should be invalid)", CS)
+	}
+}
+
+func TestSatisfiable(t *testing.T) {
+	SatValid(t)
+	SatInvalid(t)
+}
