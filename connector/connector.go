b07cab946bd44896b991f529bb6427c392808211
Shane Aston <astonshane@gmail.com>
2015-09-29 15:14:48 -0400

    full parsing for inputs that don't contain parens()

--- connector/connector.go
+++ connector/connector.go
@@ -1,6 +1,9 @@
-package connector
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 //Connector is a class to form a parseable structure from
 type Connector struct {
@@ -17,4 +20,47 @@ func (c Connector) String() string {
 		return fmt.Sprintf("{%s: %s}", c.Type, c.Children[0])
 	}
 	return fmt.Sprintf("{%s: %s}", c.Type, c.Children)
+}
+
+//Parse parses a plaintext line into a Connector sequence
+func Parse(plaintext string) Connector {
+	plaintext = strings.Replace(plaintext, " ", "", -1)
+
+	//special cases for dealing with literals (or negations of literals)
+	if len(plaintext) == 1 {
+		return Connector{Type: "Literal", Literal: plaintext}
+	} else if len(plaintext) == 2 {
+		plaintext = strings.Trim(plaintext, "~")
+		return Connector{Type: "Neg", Children: []Connector{Connector{Type: "Literal", Literal: plaintext}}}
+	}
+
+	connectors := make(map[string]string)
+	connectors["^"] = "And"
+	connectors["v"] = "Or"
+	connectors["<->"] = "Equiv"
+	connectors["->"] = "Imp"
+
+	//simple cases: no parens...
+	if !strings.Contains(plaintext, "(") {
+
+		for key, op := range connectors {
+			if strings.Contains(plaintext, key) {
+				splitPlain := strings.Split(plaintext, key)
+				children := []Connector{}
+				for _, child := range splitPlain {
+					children = append(children, Parse(child))
+				}
+				return Connector{Type: op, Children: children}
+			}
+		}
+	}
+
+	return Connector{}
+}
+
+func main() {
+	cases := []string{"A", "B", "~A", "A^B", "~A^B", "AvB", "Av~B", "A->B", "A<->B"}
+	for _, c := range cases {
+		fmt.Println(Parse(c))
+	}
 }
