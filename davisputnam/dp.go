9460715bcb66c799cd1282b5f859c7c78617cb21
Shane Aston <astonshane@gmail.com>
2016-04-06 10:40:09 -0400

    initial parsing of wolfram api for cnf conversions

--- davisputnam/dp.go
+++ davisputnam/dp.go
@@ -4,11 +4,14 @@ import (
 	"bufio"
 	"davisputnam/clause"
 	"davisputnam/clauseset"
-	"davisputnam/connector"
+	//"davisputnam/connector"
 	"fmt"
 	"log"
 	"os"
 	"strings"
+	"net/http"
+	"io/ioutil"
+	"regexp"
 )
 
 /*
@@ -75,6 +78,54 @@ func FindValidity(CS clauseset.ClauseSet) {
 	}
 }
 
+func clean(sentence string) string {
+	result := strings.Replace(sentence, "<->", "+xnor+", -1)
+	result = strings.Replace(result, "->", "+implies+", -1)
+	result = strings.Replace(result, "v", "+or+", -1)
+	result = strings.Replace(result, "^", "+and+", -1)
+	result = strings.Replace(result, "~", "+not+", -1)
+	return result
+}
+
+func getCNF(sentence string) clause.Clause {
+	c := clause.Clause{}
+	url := fmt.Sprintf("http://api.wolframalpha.com/v1/query?input=BooleanConvert[%s,%%22CNF%%22]&appid=2Y4TEV-W2AETK4T5K", clean(sentence))
+	fmt.Println(url)
+	response, err := http.Get(url)
+	if err != nil {
+        fmt.Printf("%s", err)
+        os.Exit(1)
+    } else {
+        defer response.Body.Close()
+        contents, err := ioutil.ReadAll(response.Body)
+        if err != nil {
+            fmt.Printf("%s", err)
+            os.Exit(1)
+        }
+        //fmt.Printf("%s\n", string(contents))
+		lines := strings.Split(string(contents), "\n")
+		found_first := false
+
+		for _, line := range lines {
+			//fmt.Printf("%q\n", line)
+			if strings.Contains(line, "<plaintext>") {
+				if !found_first {
+					found_first = true
+				}else{
+					//fmt.Println(line)
+					//r, _ := regexp.Compile(">([A-Z\\(\\)\\s]+)<")
+					re := regexp.MustCompile("<plaintext>(.*)</plaintext>")
+					cnf := re.FindStringSubmatch(line)[1]
+					fmt.Printf("cnf: %s\n", cnf)
+					break
+				}
+			}
+		}
+    }
+
+	return c
+}
+
 //ConstructCS reads in premisies from a file and returns its ClauseSet
 func ConstructCS(file string) clauseset.ClauseSet {
 	newCS := clauseset.ClauseSet{}
@@ -90,15 +141,17 @@ func ConstructCS(file string) clauseset.ClauseSet {
 	contin := true
 	for contin {
 		line := scanner.Text()
-		con := connector.Parse(line)
-		con = con.ToCNF()
+
 		if !scanner.Scan() {
-			con = con.Negate()
-			con = con.PropagateNegations()
+			line = fmt.Sprintf("~(%s)", line)
 			contin = false
 		}
-		cs := clauseset.ConstructCS(con)
-		newCS = clauseset.Combine(newCS, cs)
+		fmt.Println(line)
+
+		c := getCNF(line)
+
+
+		newCS.Append(c)
 	}
 
 	return newCS
@@ -111,6 +164,6 @@ func main() {
 	}
 	CS := ConstructCS(os.Args[1])
 	fmt.Println(CS)
-	FindValidity(CS)
+	//FindValidity(CS)
 
 }
